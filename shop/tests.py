a483709d4eb1505774aed863723e4c47a1867aaa
Maxim <reallowcut@gmail.com>
2023-09-23 21:19:18 +0300

    add unit tests for shop and users + some bootstrap shit

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,64 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+import io
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Product, Basket
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+    def test_category_str_method(self):
+        category = Category.objects.create(name='TestCategory', slug='test-category')
+        self.assertEqual(str(category), 'TestCategory')
+
+
+class ProductModelTest(TestCase):
+    def test_product_str_method(self):
+        category = Category.objects.create(name='TestCategory', slug='test-category')
+        product = Product.objects.create(category=category, name='TestProduct', slug='test-product', price=10.0)
+        self.assertEqual(str(product), 'TestProduct')
+
+
+class ProductsOfCategoryViewTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='TestCategory', slug='test-category')
+        image = Image.new('RGB', (100, 100))
+        tmp_file = io.BytesIO()
+        image.save(tmp_file, format='JPEG')
+        tmp_file.seek(0)
+        self.product = Product.objects.create(
+            category=self.category,
+            name='TestProduct',
+            slug='test-product',
+            price=10.0,
+            image=SimpleUploadedFile("test_product.jpg", tmp_file.read()),
+        )
+        self.url = reverse('shop:products_of_category', args=['test-category'])
+
+    def test_products_of_category_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/products_of_category.html')
+
+        products = response.context['products']
+        product = products[0]
+
+        self.assertTrue(product.image)
+        self.assertTrue(product.image.url)
+
+
+class BasketPageViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+        self.category = Category.objects.create(name='TestCategory', slug='test-category')
+        self.product = Product.objects.create(category=self.category, name='TestProduct', slug='test-product',
+                                              price=10.0)
+        self.basket = Basket.objects.create(user=self.user, product=self.product, quantity=2)
+        self.url = reverse('shop:basket_page')
+
+    def test_basket_page_view(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/baskets.html')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserRegisterViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register')
+
+    def test_register_view_get(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_register_view_post_valid_form(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+        response = self.client.post(self.register_url, data)
+        self.assertEqual(response.status_code,
+                         302)
+        self.assertRedirects(response, reverse('login'))
+
+    def test_register_view_post_invalid_form(self):
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': '',
+            'email': 'johndoe@example.com',
+            'password1': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+        response = self.client.post(self.register_url, data)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context['form']
+        self.assertTrue('username' in form.errors)
+        self.assertEqual(form.errors['username'], ['Обязательное поле.'])
